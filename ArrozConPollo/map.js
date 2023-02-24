1930a3e95ee54d595bd962b60655f5359bec27e8
TekketsuHen <vjamet15@gmail.com>
2023-02-24 17:14:14 +0100

    chasnges

--- ArrozConPollo/map.js
+++ ArrozConPollo/map.js
@@ -1,8 +1,6 @@
 
 class HeatMap {
-    constructor(height, width){
-        this.height = height;
-        this.width = width;    
+    constructor(){
     }
     update(gameState) {
         this.gameState = gameState;
@@ -10,6 +8,7 @@ class HeatMap {
         this.enemy = gameState.players[(gameState.id + 1) % 2];
         this.ValueTile = [];
         this.currentWorkersPos = [];
+        this.map = [];
         initializeMap();
 
     }
@@ -21,7 +20,7 @@ class HeatMap {
 
 
     initializeMap(gameMap) {
-      let map = [];
+      this.map = [];
       for (let y = 0; y < gameMap.height; y++) {
         for (let x = 0; x < gameMap.width; x++) {
           const cell = gameMap.getCell(x, y);
@@ -48,17 +47,41 @@ class HeatMap {
       }
       return typeofTile;
      }
-    mostEfficientTile(){
-        //TOP-LEFT remove adjacent [x-1, y], [x, y-1]
-        //TOP-RIGHT remove adjacent [x+1, y] [x, y-1]
-        //BOTTOM-LEFT remove adjacent [x, y+1] [x-1, y]
-        //BOTTOM-RIGHT remove adjacent [x+1, y], [x, y+1]
-        for (let y = 0; y < gameMap.height; y++) {
-          for (let x = 0; x < gameMap.width; x++) {
-            const adjacentCell = [[x+1, y], [x, y+1], [x-1, y], [x, y-1]];
-            map[y][x] = ;     
+    mostEfficientTile(map){
+      
+      //TOP-LEFT remove adjacent [x-1, y], [x, y-1]
+      //TOP-RIGHT remove adjacent [x+1, y] [x, y-1]
+      //BOTTOM-LEFT remove adjacent [x, y+1] [x-1, y]
+      //BOTTOM-RIGHT remove adjacent [x+1, y], [x, y+1]
+      for (let y = 0; y < gameMap.height; y++) {
+        for (let x = 0; x < gameMap.width; x++) {
+          let current = map[x][y];
+          let adjacentCells = [];
+
+          if(x == 0 || y== 0 || y == gameMap.height || x == gameMap.width ){
+            if(x == 0)
+              adjacentCells.push(map[x+1][y]);
+            if(y == 0)
+              adjacentCells.push(map[x][y+1]);
+            if(x == gameMap.width)
+              adjacentCells.push(map[x-1][y]);
+            if(y == gameMap.height)
+              adjacentCells.push(map[x][y-1]);
+
+            adjacentCells.forEach(adjCell => {if(adjCell === map[x][y]){current++} });
           }
-        }
+          else{
+            adjacentCells = [map[x+1][y], map[x][y+1], map[x-1][y], map[x][y-1]];
+            adjacentCells.forEach(adjCell => {if(adjCell === map[x][y]){current++} });
+          }
+
+          map[x][y] = current;
+          if(map[x][y] = 5 || 4){
+            x.push(this.ValueTile);
+          }
+     }
+}
+    
 /*           let adjacentCell = 
           if (x == 0) // dont check [x-1, y]
           switch (true) {
@@ -82,5 +105,3 @@ class HeatMap {
           }
           resourceTiles.push(cell);
           */
-    }
-} 
